58ea502086af9903600e02b06adb820d8988a5fb
Your Name <you@example.com>
2023-03-15 18:34:06 +0300

    HW001

---
+++ PythonSeminars/main.py
@@ -0,0 +1,47 @@
+#Комментарии
+
+#print(5)
+
+"""
+print(5)
+print(5)
+print(5)
+"""
+
+# print(5)
+# print(5)
+# ctrl + k --> ctrl + c
+# ctrl + k --> ctrl + u
+
+#Функция ввода неправильная
+
+# print('Input first number: ')
+# a = input()
+# print(a)
+# b = input('Input second number')
+
+# print(a, ' + ', b, ' = ', a + b)
+
+#Функция ввода правильная
+
+# print('Input first number: ')
+# a = int(input())
+# b = int(input('Input second number'))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+
+#Количество символов в дробном числе
+
+# a = 1.12234
+# b = 2.33333
+# print(round(a*b, 2))
+
+# a = int(input('Input first number: '))
+# b = int(input('Input second number: '))
+# c = int(input('Input third number: '))
+# d = a + b + c
+# print(f"Your number is: {d}") 
+
+
+
